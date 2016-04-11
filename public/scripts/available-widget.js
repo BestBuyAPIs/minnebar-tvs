e42908cab930184be63b19f2cd0a11d8526184e5
Dan Flettre <fletd01@yahoo.com>
2016-04-11 17:06:34 -0500

    semistandard

--- public/scripts/available-widget.js
+++ public/scripts/available-widget.js
@@ -1,28 +1,29 @@
+/* globals moment, $ */
 'use strict';
 
 window.widgets = {
   'gsw_hub_map': {
     name: 'Hub Map',
-    load: function($el){
+    load: function ($el) {
       $el.html('<img src="/maps/map_hub.jpg" style="width:100%">');
     }
   },
   'gsw_b1_map': {
     name: 'B1 Map',
-    load: function($el){
+    load: function ($el) {
       $el.html('<img src="/maps/map_b1.png" style="width:100%">');
     }
   },
   'gsw_session_list': {
     name: 'Session List',
-    load: function($el){
+    load: function ($el) {
       var getSession = function () {
         $.getJSON('/session-list.json', function (data) {
           var items = [];
-          $.each( data, function( key, val ) {
-            items.push( "<li id='" + key + "'>" + JSON.stringify(val) + "</li>" );
+          $.each(data, function (key, val) {
+            items.push("<li id='" + key + "'>" + JSON.stringify(val) + '</li>');
           });
-          $el.html( "<ul>" + items.join( "" ) + "</ul>");
+          $el.html('<ul>' + items.join('') + '</ul>');
         });
       };
       getSession();
@@ -31,14 +32,14 @@ window.widgets = {
   },
   'gsw_recent_tweets': {
     name: 'Recent Tweets',
-    load: function($el){
+    load: function ($el) {
       var getTweets = function () {
         $.getJSON('/recent-tweets.json', function (data) {
           var items = [];
-          $.each( data, function( key, val ) {
-            items.push( "<li id='" + key + "'>" + JSON.stringify(val) + "</li>" );
+          $.each(data, function (key, val) {
+            items.push("<li id='" + key + "'>" + JSON.stringify(val) + '</li>');
           });
-          $el.html( "<ul>" + items.join( "" ) + "</ul>");
+          $el.html('<ul>' + items.join('') + '</ul>');
         });
       };
       getTweets();
@@ -47,15 +48,15 @@ window.widgets = {
   },
   'gsw_qr_codes': {
     name: 'QR Codes',
-    load: function($el){
+    load: function ($el) {
       $el.html('<img src="/images/qr.png" style="width:100%">');
     }
   },
   'gsw_clock': {
     name: 'Clock',
-    load: function($el){
+    load: function ($el) {
       $el.fitText();
-      setInterval(function update() {
+      setInterval(function update () {
         $el.html(moment().format('H:mm:ss'));
       });
     }
