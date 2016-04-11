87e787c1d20d82bb8abf39155be00f2738b5339f
Eric Caron <eric.caron@bestbuy.com>
2016-04-11 12:53:01 -0500

    Wiring up the proof-of-concept for the clock

--- public/scripts/available-widget.js
+++ public/scripts/available-widget.js
@@ -1,11 +1,38 @@
 'use strict';
 
-/* exported widgets */
 window.widgets = {
-  'gsw_hub_map': { name: 'Hub Map' },
-  'gsw_b1_map': { name: 'B1 Map' },
-  'gsw_session_list': { name: 'Session List' },
-  'gsw_recent_tweets': { name: 'Recent Tweets' },
-  'gsw_qr_codes': { name: 'QR Codes' },
-  'gsw_clock': { name: 'Clock' }
+  'gsw_hub_map': {
+    name: 'Hub Map',
+    load: function(){
+    }
+  },
+  'gsw_b1_map': {
+    name: 'B1 Map',
+    load: function(){
+    }
+  },
+  'gsw_session_list': {
+    name: 'Session List',
+    load: function(){
+    }
+  },
+  'gsw_recent_tweets': {
+    name: 'Recent Tweets',
+    load: function(){
+    }
+  },
+  'gsw_qr_codes': {
+    name: 'QR Codes',
+    load: function(){
+    }
+  },
+  'gsw_clock': {
+    name: 'Clock',
+    load: function(){
+      $('#gsw_clock').fitText(1.3);
+      setInterval(function update() {
+        $('#gsw_clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
+      });
+    }
+  }
 };
